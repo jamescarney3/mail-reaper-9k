2b869a8f6d478dffcdbf54cd60bd0200d346a178
James Carney <jmcarneyiii@gmail.com>
2023-03-03 18:03:30 -0500

    FIX ensure install script creates an env file and sets a variable if needed

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,9 @@
 echo installing Mail Reaper 9000
 clasp login
 clasp create --title "Mail Reaper 9000" --type standalone
+touch .env
+read -p "set EXAMPLE_ENV_VAR:" example_env_var
+echo "EXAMPLE_ENV_VAR=$example_env_var" >> .env
 yarn install
 yarn run build
 clasp push
